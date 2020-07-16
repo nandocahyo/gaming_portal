5a16e2b25f455e4431a6f4fe53bdedfd8372d9b7
nandocahyo <nandocahyo77@gmail.com>
2020-07-16 20:17:43 +0700

    slicing admin

---
+++ resources/views/includes/admin/script.blade.php
@@ -0,0 +1,17 @@
+<!-- Bootstrap core JavaScript-->
+    <script src="{{url('backend/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{url('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{url('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="{{url('backend/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{url('backend/vendor/datatables/jquery.dataTables.js')}}"></script>
+    <script src="{{url('backend/vendor/datatables/dataTables.bootstrap4.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{url('backend/js/sb-admin.min.js')}}"></script>
+
+    <!-- Demo scripts for this page-->
+    <script src="{{url('backend/js/demo/datatables-demo.js')}}"></script>
+    <script src="{{url('backend/js/demo/chart-area-demo.js')}}"></script>
---
+++ resources/views/includes/admin/style.blade.php
@@ -0,0 +1,11 @@
+ <!-- Bootstrap core CSS-->
+ <link href="{{url('backend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+<!-- Custom fonts for this template-->
+<link href="{{url('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
+<!-- Page level plugin CSS-->
+<link href="{{url('backend/vendor/datatables/dataTables.bootstrap4.css')}}" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="{{url('backend/css/sb-admin.css')}}" rel="stylesheet">
